d2fdbaa622a2c3d12e9735511544aba48ea04a71
RABUNTHOEUN <rabunthoeun7777@gmail.com>
2025-05-10 16:25:58 +0700

    add translate login and register

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -10,6 +10,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
 import { PasswordInput } from '@/components/ui/password-input';
+import useTranslation from '@/hooks/use-translation';
 
 type LoginForm = {
     email: string;
@@ -36,14 +37,16 @@ export default function Login({ status, canResetPassword }: LoginProps) {
         });
     };
 
+    const {t} = useTranslation();
+
     return (
-        <AuthLayout title="Log in to your account" description="Enter your email and password below to log in">
+        <AuthLayout title={t("Log in to your account")} description={t("Enter your email and password below to log in to your account")}>
             <Head title="Log in" />
 
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
                     <div className="grid gap-2">
-                        <Label htmlFor="email">Email address</Label>
+                        <Label htmlFor="email">{t("Email address")}</Label>
                         <Input
                             id="email"
                             type="email"
@@ -53,17 +56,17 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                             autoComplete="email"
                             value={data.email}
                             onChange={(e) => setData('email', e.target.value)}
-                            placeholder="email@example.com"
+                            placeholder={t("email@example.com")}
                         />
                         <InputError message={errors.email} />
                     </div>
 
                     <div className="grid gap-2">
                         <div className="flex items-center">
-                            <Label htmlFor="password">Password</Label>
+                            <Label htmlFor="password">{t("Password")}</Label>
                             {canResetPassword && (
                                 <TextLink href={route('password.request')} className="ml-auto text-sm" tabIndex={5}>
-                                    Forgot password?
+                                    {t("Forgot password?")}
                                 </TextLink>
                             )}
                         </div>
@@ -75,7 +78,7 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                             autoComplete="current-password"
                             value={data.password}
                             onChange={(e) => setData('password', e.target.value)}
-                            placeholder="Password"
+                            placeholder={t("Password")}
                         />
                         <InputError message={errors.password} />
                     </div>
@@ -88,12 +91,12 @@ export default function Login({ status, canResetPassword }: LoginProps) {
                             onClick={() => setData('remember', !data.remember)}
                             tabIndex={3}
                         />
-                        <Label htmlFor="remember">Remember me</Label>
+                        <Label htmlFor="remember">{t("Remember me")}</Label>
                     </div>
 
                     <Button type="submit" className="mt-4 w-full" tabIndex={4} disabled={processing}>
                         {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                        Log in
+                        {t("Log in")}
                     </Button>
                 </div>
 
