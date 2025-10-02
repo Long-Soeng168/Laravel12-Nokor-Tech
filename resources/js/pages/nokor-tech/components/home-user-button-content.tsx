ef45d7625e3a4537573e25637b6f4da6b1a99c1c
Long-Soeng167 <generalpurpose167@gmail.com>
2025-10-02 16:48:42 +0700

    dd

--- resources/js/pages/nokor-tech/components/home-user-button-content.tsx
+++ resources/js/pages/nokor-tech/components/home-user-button-content.tsx
@@ -4,7 +4,7 @@ import { useMobileNavigation } from '@/hooks/use-mobile-navigation';
 import useTranslation from '@/hooks/use-translation';
 import { type User } from '@/types';
 import { Link } from '@inertiajs/react';
-import { LogOut, Settings } from 'lucide-react';
+import { LayoutIcon, LogOut, Settings } from 'lucide-react';
 
 interface HomeUserButtonContentProps {
     user: User;
@@ -24,6 +24,12 @@ export function HomeUserButtonContent({ user }: HomeUserButtonContentProps) {
             <DropdownMenuSeparator />
 
             <DropdownMenuGroup>
+                <DropdownMenuItem asChild>
+                    <Link className="block w-full" href={`/dashboard`} as="button" prefetch onClick={cleanup}>
+                        <LayoutIcon className="mr-2" />
+                        {t('Dashboard')}
+                    </Link>
+                </DropdownMenuItem>
                 <DropdownMenuItem asChild>
                     <Link className="block w-full" href={route('profile.edit')} as="button" prefetch onClick={cleanup}>
                         <Settings className="mr-2" />
